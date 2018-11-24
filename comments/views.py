f57cb054c44caa5ba1091d102f49b905c576e3f7
sunyinkai <1091491336@qq.com>
2018-11-24 14:54:28 +0800

    11_24 14:54

--- comments/views.py
+++ comments/views.py
@@ -14,14 +14,13 @@ from django.db import models
 def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        print('hellllllllllllllllllo')
         print(form.is_valid())
         if form.is_valid():
             print('--------in login--------')
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             t = User.objects.filter(username=username)
-            user = User(username=username)  # 如果用户名不存在保存用户,否则就使用用户
+            user = User(username=username)  # 如果用户名不存在,注册保存用户,否则就使用用户
             if len(t):
                 user = User.objects.get(username=username)
             else:
@@ -33,8 +32,10 @@ def login(request):
     return render(request, 'login.html')
 
 
-def index(request):  # 如果访问localhost:8000/polls/index/
+def index(request):  # 如果访问localhost:8000/polls/index/由这个视图函数处理
     is_login = request.session.get('is_login', False)
+    if is_login is False:  # 如果没有登录,跳转到登录界面
+        return redirect('/comments/login/')
     if is_login:  # 如果已经登录
         # if forms validate_on_submit,save to db,return redirect(/index/)
         if request.method == 'POST':
@@ -45,26 +46,20 @@ def index(request):  # 如果访问localhost:8000/polls/index/
                 username = request.session.get('username')
                 user = User(username=username, id=user_id)
                 post = Post(body=body, author=user)
-                post.save()
-                # 将表单中的数据提取出来,并且存储到数据库中
-
-                print('post information:----------------------')
-                print('now in post')
-                print('the post content is:\n %s' % (body))
-                print('the user_id is: %d' % (user_id))
-                print('end post:------------------------------')
-            return render(request, 'index.html', {
-                'form': request.session.get('username')})  # why use request.session.usernam will raise problem
-        return render(request, 'index.html')
-
-    else:
-        return redirect('/login/')  # 跳转到登录界面
-
+                post.save()  # 将表单中的数据提取出来,并且存储到数据库中
+                return redirect('/comments/index/')
+            # 打印存储到数据库中的信息
+            # print('post information:----------------------')
+            # print('now in post')
+            # print('the post content is:\n %s' % (body))
+            # print('the user_id is: %d' % (user_id))
+            # print('end post:------------------------------')
+        posts = Post.objects.all()  # 注意:这里是显示所有的动态页面:
+        return render(request, 'index.html', {'form': request.session.get('username'), 'posts': posts, })
 
 def register(request):
-    return render(request, 'register.html')
-
+        return render(request, 'register.html')
 
 def logout(request):
-    pass
-    return redirect("/index/")
+        pass
+        return redirect("/index/")
